81408464aabcbabcf01ec9dafb5f525005bb621c
Mohamed Lafir umair <99977258+DevoUmair@users.noreply.github.com>
2025-04-22 03:22:56 +0500

    updates

--- controllers/User.controllers.js
+++ controllers/User.controllers.js
@@ -1,4 +1,5 @@
 import User from "../models/User.model.js";
+import jwt from 'jsonwebtoken';
 import {
   generateAccessToken,
   generateRefreshToken,
--- routes/User.router.js
+++ routes/User.router.js
@@ -1,16 +1,9 @@
 import { Router } from "express";
-import {
-  register,
-  login,
-  refreshAccessToken,
-  logout,
-} from "../controllers/User.controllers.js";
+import { register, login} from "../controllers/User.controllers.js";
 
 const router = Router();
 
 router.post("/register", register);
 router.post("/login", login);
-router.get("/token/refresh", refreshAccessToken);
-router.post("/logout", logout);
 
 export default router;
