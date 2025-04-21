3c90660594e47ed62a1d1be3d6fa3a90ffc6acf2
saim04 <saimfayyaz124@gmail.com>
2025-04-22 04:50:19 +0500

    Updates

--- controllers/User.controllers.js
+++ controllers/User.controllers.js
@@ -1,5 +1,5 @@
 import User from "../models/User.model.js";
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 import {
   generateAccessToken,
   generateRefreshToken,
@@ -121,5 +121,23 @@ export const logout = async (req, res) => {
   } catch (err) {
     res.clearCookie("refreshToken", { ...cookieOptions, maxAge: 0 });
     res.status(403).json({ message: "Invalid token on logout" });
+  }
+};
+
+// Admin Controllers
+export const getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+
+    if (!users) {
+      return res.status(404).json({ message: "No users found." });
+    }
+
+    return res.status(200).json(users);
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ message: "Server error. Unable to fetch users." });
   }
 };
