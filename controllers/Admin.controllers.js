57e033f3d73512fceb34e06fb9da4350aa9acb96
Mohamed Lafir umair <99977258+DevoUmair@users.noreply.github.com>
2025-04-22 19:57:14 +0500

    mongoose connect

--- controllers/Admin.controllers.js
+++ controllers/Admin.controllers.js
@@ -35,3 +35,42 @@ export const getAllAccounts = async (req, res) => {
       .json({ message: "Server error. Unable to fetch accounts." });
   }
 };
+
+
+export const login = async (req, res) => {
+    try {
+      const { email, password } = req.body;
+
+      const user = await User.findOne({ email });
+  
+      if (!user || !(await user.comparePassword(password))) {
+        return res.status(400).json({ message: "Invalid email or password" });
+      }
+ 
+      if (user.role !== "admin") {
+        return res.status(403).json({ message: "Access denied. Admins only." });
+      }
+  
+      const accessToken = generateAccessToken(user);
+      const refreshToken = generateRefreshToken(user);
+  
+      user.refreshToken = refreshToken;
+      await user.save();
+  
+      res
+        .cookie("refreshToken", refreshToken, cookieOptions)
+        .status(200)
+        .json({
+          message: "Admin login successful",
+          accessToken,
+          user: {
+            id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            role: user.role,
+          },
+        });
+    } catch (err) {
+      res.status(500).json({ message: "Server error", error: err.message });
+    }
+  };
