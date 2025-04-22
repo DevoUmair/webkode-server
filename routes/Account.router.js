d1b96acd6a8a72f72825af14a69a6a98af6ba934
saim04 <saimfayyaz124@gmail.com>
2025-04-23 02:49:33 +0500

    Updates

--- routes/Account.router.js
+++ routes/Account.router.js
@@ -1,15 +1,36 @@
-import express from 'express';
-import { checkBalance , depositMoney  , createAccount, transferMoney , getAllTransaction} from '../controllers/Account.cotroller.js';
-import { verifyToken  } from '../middlewares/auth.middleware.js';
-import { verifyActiveSubscription  } from '../middlewares/subscription.middleware.js';
+import express from "express";
+import {
+  checkBalance,
+  depositMoney,
+  createAccount,
+  transferMoney,
+  getAllTransaction,
+} from "../controllers/Account.cotroller.js";
+import { verifyToken } from "../middlewares/auth.middleware.js";
+import { verifyActiveSubscription } from "../middlewares/subscription.middleware.js";
 
 const router = express.Router();
 
 // Apply middleware before the controller
-router.post('/check-balance', verifyToken,  verifyActiveSubscription ,  checkBalance);
-router.post('/deposit', verifyToken, verifyActiveSubscription , depositMoney);
-router.post('/create-account', verifyToken, verifyActiveSubscription ,  createAccount);
-router.post('/transfer-money', verifyToken, verifyActiveSubscription ,  transferMoney);
-router.get('/transaction', verifyToken, verifyActiveSubscription , getAllTransaction);
+router.post(
+  "/check-balance",
+  verifyToken,
+  verifyActiveSubscription,
+  checkBalance
+);
+router.post("/deposit", verifyToken, verifyActiveSubscription, depositMoney);
+router.post("/create-account", verifyToken, createAccount);
+router.post(
+  "/transfer-money",
+  verifyToken,
+  verifyActiveSubscription,
+  transferMoney
+);
+router.get(
+  "/transaction",
+  verifyToken,
+  verifyActiveSubscription,
+  getAllTransaction
+);
 
 export default router;
